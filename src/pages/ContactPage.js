5edd9840e1aadecffc0d86a208444d8dec3c2caa
Jalen Lewis <jalenlew1995@gmail.com>
2020-12-08 21:25:35 -0600

    Final push

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,94 +1,21 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Hero from '../components/Hero';
 import Content from '../components/Content';
-import Axios from 'axios';
+import Hero from '../components/Hero';
+import Jalen from '../assets/images/Jalen.jpg';
+
 
 class ContactPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            email: '',
-            message: '',
-            disabled: '',
-            emailSent: '',
-        }
-    }
-
-    handleChange = (event) => {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        })
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(event.target);
-
-        this.setState({
-            disabled: true
-        });
-
-        Axios.post('http://localhost:3000/api/email', this.state)
-            .then(res => {
-                if (res.data.success) {
-                    this.setState({
-                        disabled: false,
-                        emailSent: true
-                    });
-                } else {
-                    this.setState({
-                        disabled: false,
-                        emailSent: false
-                    });
-                }
-            })
-            .catch(err => {
-                console.log(err);
-
-                this.setState({
-                    disabled: false,
-                    emailSent: false
-                });
-            })
-    }
-    
     render() {
         return (
             <div>
-                <Hero title={this.props.title} />
+               <Hero title={this.props.title} />
 
-                <Content>
-                    <Form onSubmit={this.handleSubmit}>
-                        <Form.Group>
-                            <Form.Label htmlFor="full-name">Full Name</Form.Label>
-                            <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
-                        </Form.Group>
-
-                        <Form.Group>
-                            <Form.Label htmlFor="email">Email</Form.Label>
-                            <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
-                        </Form.Group>
-
-                        <Form.Group>
-                            <Form.Label htmlFor="message">Message</Form.Label>
-                            <Form.Control id="message" name="message" as="textarea"rows="3" value={this.state.message} onChange={this.handleChange} />
-                        </Form.Group>
-
-                        <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled}>
-                            Send
-                        </Button>
-
-                        {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
-                        {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>}
-                    </Form>
-                </Content>
+               <Content>
+                   <img src={Jalen} alt="Profile Picture" />;
+                   <a href="https://github.com/jlew2529">Github Profile</a>
+                   <a href="https://www.linkedin.com/in/jalen-lewis-288065144/">LinkedIn Profile</a>
+                   <a href="https://docs.google.com/document/d/13XUQPoc1jwssPCUlkO2DcIefwpDveUDkLmgKbGH1uxI/edit?usp=sharing">Resume</a>
+               </Content>
             </div>
         );
     }
